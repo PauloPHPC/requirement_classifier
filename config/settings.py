c231ca9cb9538e454e61c0cec0e0b676dced9582
Paulo Henrique Pianissola de Cerqueira <109831929+PauloPHPC@users.noreply.github.com>
2025-11-19 18:07:22 +0000

    Refactor DATABASES setting for SQLite configuration

--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': 'django.db.backends.sqlite3',
+    'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
     'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
