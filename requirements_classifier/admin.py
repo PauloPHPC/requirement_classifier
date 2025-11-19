c36bfe6ff716c0d72a6ba923252d5ce4fb6ed088
Paulo Henrique Pianissola de Cerqueira <109831929+PauloPHPC@users.noreply.github.com>
2025-11-19 18:06:00 +0000

    Register Requirements and Documents models in admin

--- requirements_classifier/admin.py
+++ requirements_classifier/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from requirements_classifier.models import Requirements, Documents
 # Register your models here.
+
+admin.site.register(Documents)
+admin.site.register(Requirements)
