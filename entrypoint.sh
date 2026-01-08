6d03d0f30c16bcf06f89e3cd5ee34aca128d3c3e
Paulo Henrique Pianissola de Cerqueira <109831929+PauloPHPC@users.noreply.github.com>
2026-01-08 20:34:48 +0000

    Change SECRET_KEY to DJANGO_SECRET_KEY in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,11 +26,16 @@ else
 fi
 
 touch "$ENV_FILE"
-if ! grep -qE '^SECRET_KEY=' "$ENV_FILE"; then
-  echo ">> Gerando SECRET_KEY no .env"
+
+if ! grep -qE '^DJANGO_SECRET_KEY=' "$ENV_FILE"; then
+  echo ">> Gerando DJANGO_SECRET_KEY no .env"
   SECRET_KEY="$(python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())")"
-  echo "SECRET_KEY=$SECRET_KEY" >> "$ENV_FILE"
+  printf 'DJANGO_SECRET_KEY=%s\n' "$SECRET_KEY" >> "$ENV_FILE"
+else
+  SECRET_KEY="$(grep -E '^DJANGO_SECRET_KEY=' "$ENV_FILE" | head -n1 | cut -d= -f2-)"
 fi
+
+export DJANGO_SECRET_KEY="$SECRET_KEY"
 
 set -a
 # shellcheck disable=SC1090
