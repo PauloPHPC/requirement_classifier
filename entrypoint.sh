43937a575a89ae90055bd8172df0eaf4ab21aa5d
Paulo Henrique Pianissola de Cerqueira <109831929+PauloPHPC@users.noreply.github.com>
2026-01-08 20:47:13 +0000

    Fix DJANGO_SECRET_KEY formatting in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,17 +30,12 @@ touch "$ENV_FILE"
 if ! grep -qE '^DJANGO_SECRET_KEY=' "$ENV_FILE"; then
   echo ">> Gerando DJANGO_SECRET_KEY no .env"
   SECRET_KEY="$(python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())")"
-  printf 'DJANGO_SECRET_KEY=%s\n' "$SECRET_KEY" >> "$ENV_FILE"
+  printf "DJANGO_SECRET_KEY='%s'\n" "$SECRET_KEY" >> "$ENV_FILE"
 else
-  SECRET_KEY="$(grep -E '^DJANGO_SECRET_KEY=' "$ENV_FILE" | head -n1 | cut -d= -f2-)"
+  SECRET_KEY="$(grep -E '^DJANGO_SECRET_KEY=' "$ENV_FILE" | head -n1 | cut -d= -f2- | sed -e \"s/^'//\" -e \"s/'$//\")"
 fi
 
 export DJANGO_SECRET_KEY="$SECRET_KEY"
-
-set -a
-# shellcheck disable=SC1090
-source "$ENV_FILE"
-set +a
 
 python manage.py collectstatic --noinput || true
 python manage.py migrate --noinput || true
